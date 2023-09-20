d6ffa1c734a2ec11c41f23cdbe6718cc687e468c
unknown <peichl294@gmail.com>
2023-09-20 20:21:00 +0200

    Working

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -16,86 +16,94 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            /*
-             * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
-             * 
-             * ZADANI
-             * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
-             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
-             * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
-             * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
-             * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
-             * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
-             * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
-             *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
-             * 7) Vypis promennou result do konzole
-             * 
-             * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
-             * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
-             * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
-             * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
-             *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
-             * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
-             */
+            //Defining shit
+            List<string> realOperators = new List<string>();
+            List<string> Numbs = new List<string>();
+            List<float> NumbFloats = new List<float>();
+            float result = 0;
+            float partialResult = 0;
 
-            //Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
 
-            float check; float FloInp2; float FloInp1;
-            Console.WriteLine("Tohle je kalkulacka, napis prvni cislo");
-            while (true)
+            string input = Console.ReadLine();
+
+            //Making sure there is no BS
+            string inputMod = input.Replace('+', 'P').Replace('-', 'M').Replace('*', 'T').Replace('/', 'D');
+            if (input.Last() == '=') { input = input.Remove(input.Length - 1); }
+
+            //Finding the numbers in input
+            String[] seperator = { "M", "P", "T", "D" };
+            int count = 50;
+            String[] strlist = inputMod.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
+            foreach (String s in strlist)
             {
-                string input1 = Console.ReadLine();
-                bool success1 = float.TryParse(input1, out check);
-                if (success1 == true)
-                {
-                    FloInp1 = float.Parse(input1);
-                    Console.WriteLine("A ted napis druhe cislo");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("To neni cislo, napis cislo");
-                }
-            }
-            while (true)
-                {
-                string input2 = Console.ReadLine();
-                bool success2 = float.TryParse(input2, out check);
-                if (success2 == true)
-                {
-                    FloInp2 = float.Parse(input2);
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("To neni cislo, napis cislo");
-                }
-                
+                Numbs.Add(s);
             }
 
-            Console.WriteLine("Ted napis ciselnou operaci");
-            string znamenko = Console.ReadLine();
-            while (true)
+            //Finding the operations in input
+            for (int i = 0; i < inputMod.Length; i++)
             {
-                    if (znamenko ==  "+")
-                    {
-                        Console.WriteLine("{0} + {1} = {2}", FloInp1, FloInp2, FloInp1 + FloInp2);
-                    }
-                    if (znamenko == "-")
-                    {
-                        Console.WriteLine("{0} - {1} = {2}", FloInp1, FloInp2, FloInp1 - FloInp2);
-                    }
-                    if (znamenko == "*")
-                    {
-                        Console.WriteLine("{0} * {1} = {2}", FloInp1, FloInp2, FloInp1 * FloInp2);
-                    }
-                    if (znamenko == "/")
-                    {
-                        Console.WriteLine("{0} / {1} = {2}", FloInp1, FloInp2, FloInp1 / FloInp2);
-                    }
-                Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.         
+                char Position = inputMod[i];
+                if (Position == 'P') { realOperators.Add("+"); }
+                if (Position == 'M') { realOperators.Add("-"); }
+                if (Position == 'T') { realOperators.Add("*"); }
+                if (Position == 'D') { realOperators.Add("/"); }
             }
-            
+            //Calculating multiplication/division, Line 56 first index + 1 to not change index of first position
+            NumbFloats = Numbs.Select(float.Parse).ToList();
+            while (realOperators.Count != 0)
+            {
+                for (int i = 0; i < realOperators.Count; i++)
+                {
+                    if (realOperators[i] == "*")
+                    {
+                        partialResult = NumbFloats[i] * NumbFloats[i + 1];
+                        realOperators.Remove(realOperators[i]);
+                        NumbFloats.Remove(NumbFloats[i + 1]);
+                        NumbFloats.Remove(NumbFloats[i]);
+                        NumbFloats.Insert(i, partialResult);
+                        //result += partialResult; Mam pocit ze tohle bude potreba
+                    }
+                }
+                for (int i = 0; i < realOperators.Count; i++)
+                {
+                    if (realOperators[i] == "/")
+                    {
+                        partialResult = NumbFloats[i] / NumbFloats[i + 1];
+                        realOperators.Remove(realOperators[i]);
+                        NumbFloats.Remove(NumbFloats[i + 1]);
+                        NumbFloats.Remove(NumbFloats[i]);
+                        NumbFloats.Insert(i, partialResult);
+                    }
+                }
+                for (int i = 0; i < realOperators.Count; i++)
+                {
+                    if (realOperators[i] == "+")
+                    {
+
+                        partialResult = NumbFloats[i] + NumbFloats[i + 1];
+                        realOperators.Remove(realOperators[i]);
+                        NumbFloats.Remove(NumbFloats[i + 1]);
+                        NumbFloats.Remove(NumbFloats[i]);
+                        NumbFloats.Insert(i, partialResult);
+                    }
+                }
+                for (int i = 0; i < realOperators.Count; i++)
+                {
+                    if (realOperators[i] == "-")
+                    {
+                        partialResult = NumbFloats[i] - NumbFloats[i + 1];
+                        realOperators.Remove(realOperators[i]);
+                        NumbFloats.Remove(NumbFloats[i + 1]);
+                        NumbFloats.Remove(NumbFloats[i]);
+                        NumbFloats.Insert(i, partialResult);
+                    }
+                }
+            }
+            //Console.WriteLine(NumbFloats.Count);
+            NumbFloats.ForEach(Console.WriteLine);
+            //realOperators.ForEach(Console.WriteLine);
+            Console.ReadKey();
+
         }
     }
 }
