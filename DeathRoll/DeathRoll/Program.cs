65ba96b43bc1fc4f5bf36de06996d34ed7991b56
unknown <peichl294@gmail.com>
2023-09-23 22:50:06 +0200

    HW Done

---
+++ DeathRoll/DeathRoll/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DeathRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold = 1000; bool result = false; string text; int change; int zmena_goldu = gold; bool state = true; int int_sazka = 5000000;
+            
+            while (state == true) 
+            {
+                bool yn_check = false; 
+                Console.WriteLine("Kolik chces vsadit? Pocet goldu: {0}", zmena_goldu);
+                string sazka = Console.ReadLine();
+                int_sazka = int.Parse(sazka);
+                if (int.TryParse(sazka, out change) == true)
+                {
+                    int_sazka = int.Parse(sazka);
+                    if (int_sazka > zmena_goldu) { Console.WriteLine("Sazka prevysuje stavajici pocet goldu, zadejte odpovídající částku: "); }
+                }
+                while (int_sazka > zmena_goldu) 
+                {
+                    sazka = Console.ReadLine();
+                    if(int.TryParse(sazka, out change) == true)
+                    {
+                        int_sazka = int.Parse(sazka);
+                        if(int_sazka > zmena_goldu) { Console.WriteLine("Sazka prevysuje stavajici pocet goldu, zadejte odpovídající částku: "); }
+                    }
+                    else { Console.WriteLine("Zadej cislo"); }
+                }
+                int new_value = int_sazka;
+                Console.WriteLine("Hra zacina se sazkou {0}", int_sazka);
+                while (new_value != 1)
+                {
+                    Random rnd = new Random();
+                    int roll1 = rnd.Next(1, new_value);
+                    Console.WriteLine("Padla ti {0}, [1-{1}]", roll1, new_value);
+                    new_value = roll1;
+                    if (new_value == 1) { result = true; break; }
+                    int roll2 = rnd.Next(1, new_value);
+                    Console.WriteLine("Pocitaci padla {0}, [1-{1}]", roll2, new_value);
+                    new_value = roll2;
+                    if (new_value == 1) { result = false; break; }
+                }
+                if (result == false) { text = "Vyhral jsi"; change = 1; }
+                else { text = "Prohral jsi"; change = -1; }
+                zmena_goldu = zmena_goldu + int_sazka * change;
+                Console.WriteLine("{0} {1} goldu, takze ted mas {2}\nChces hrat znovu? (y/n)", text, int_sazka, zmena_goldu);
+                List<string> yn = new List<string>();
+                yn.Add("y"); yn.Add("n");
+                while (yn_check == false)
+                {
+                    string input = Console.ReadLine();
+                    if (yn.Contains(input) == true)
+                    {
+                        if (input == yn[0]) { yn_check = true; }
+                        if (input == yn[1]) { yn_check = false; Environment.Exit(0); }
+                    }
+                    else { Console.WriteLine("Wrong input"); }
+                }
+                Console.ReadKey();
+            } 
+        }
+    }
+}
---
+++ Kamen_Nuzky_Papir/Kamen_Nuzky_Papir/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamen_Nuzky_Papir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string confirm = "y";
+            List<string> list = new List<string>();
+            list.Add("kamen"); list.Add("papir"); list.Add("nuzky");
+            List <string> moznosti = new List<string>();
+            moznosti.Add("hra skoncila remizou"); moznosti.Add("jsi vyhral"); moznosti.Add("jsi prohral");
+            string vysledek = "";
+            double skore_hrace = 0;
+            double skore_pocitace = 0;
+            Console.WriteLine("Tohle je kamen/nuzky/papir, vyber jsi jednu ze tri: ");
+            while (true)
+            {
+                bool yn_check = false; string input = "";
+                Random rnd = new Random();
+                int pocitac_choice = rnd.Next(0, 2);
+                string pocitac_vyber = list[pocitac_choice];
+                input = Console.ReadLine();
+                while (list.Contains(input) == false)
+                {
+                    Console.WriteLine("To neni platna moznost");
+                    input = Console.ReadLine();
+                }
+                if (list.Contains(input) == true)
+                {
+                    if (pocitac_vyber == input) { vysledek = moznosti[0]; skore_hrace += 0.5; skore_pocitace += 0.5; }
+                    switch (input)
+                    {
+                        case "kamen":
+                            if (pocitac_vyber == "papir") { vysledek = moznosti[2]; skore_pocitace += 1; }
+                            if (pocitac_vyber == "nuzky") { vysledek = moznosti[1]; skore_hrace += 1; }
+                            break;
+                        case "papir":
+                            if (pocitac_vyber == "nuzky") { vysledek = moznosti[2]; skore_pocitace += 1; }
+                            if (pocitac_vyber == "kamen") { vysledek = moznosti[1]; skore_hrace += 1; }
+                            break;
+                        case "nuzky":
+                            if (pocitac_vyber == "kamen") { vysledek = moznosti[2]; skore_pocitace += 1; }
+                            if (pocitac_vyber == "papir") { vysledek = moznosti[1]; skore_hrace += 1; }
+                            break;
+                    }
+                    Console.WriteLine("Pocitac vybral {0}, takze {1} a skore je momentalne {2}:{3} (Ty:Pocitac)\nChces hrat znovu? (y/n)", pocitac_vyber, vysledek, skore_hrace, skore_pocitace);
+                    //confirm = Console.ReadLine();
+                    //if (confirm == "y"){ Console.WriteLine("Hraje se znovu, opet si vyber: "); } else { Environment.Exit(0); }
+                    List<string> yn = new List<string>();
+                    yn.Add("y"); yn.Add("n");
+                    while (yn_check == false)
+                    {
+                        string input_yn = Console.ReadLine();
+                        if (yn.Contains(input_yn) == true)
+                        {
+                            if (input_yn == yn[0]) { yn_check = true; }
+                            if (input_yn == yn[1]) { yn_check = false; Environment.Exit(0); }
+                        }
+                        else { Console.WriteLine("Wrong input"); }
+                    }
+                }
+                Console.WriteLine("Vyber si opet jednu ze 3 moznosti: ");
+                Console.ReadKey(); 
+            }
+        }
+    }
+}
