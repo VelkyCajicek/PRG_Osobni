daef7040d36ee9e25aae1db924e3c3c37b28fc0a
unknown <peichl294@gmail.com>
2023-09-27 18:27:05 +0200

    Pridane zavorky

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -20,88 +20,109 @@ namespace Calculator
             List<string> realOperators = new List<string>();
             List<string> Numbs = new List<string>();
             List<float> NumbFloats = new List<float>();
-            float result = 0;
-            float partialResult = 0;
-
+            float partialResult = 0; int firstStringPosition = 0; int secondStringPosition = 0; bool bracketCheck = false;
 
             string input = Console.ReadLine();
 
             //Making sure there is no BS
-            string inputMod = input.Replace('+', 'P').Replace('-', 'M').Replace('*', 'T').Replace('/', 'D');
             if (input.Last() == '=') { input = input.Remove(input.Length - 1); }
-
+            //Zavorky
+            string inputModBracket = input;
+            if (inputModBracket.Contains('(') == true) { bracketCheck = true; }
+            while (inputModBracket.Contains('(') == true)
+            {
+                for (int i = 0; i < inputModBracket.Length; i++)
+                {
+                    if (inputModBracket[i] == '(') { firstStringPosition = i; }
+                    if (inputModBracket[i] == ')') { secondStringPosition = i; }
+                }
+                string Bracket = inputModBracket.Substring(firstStringPosition, secondStringPosition - firstStringPosition + 1);
+                string BracketMod = Bracket.Replace('+', 'p').Replace('-', 'm').Replace('*', 't').Replace('/', 'd');
+                input = input.Replace(Bracket, BracketMod);
+                inputModBracket = inputModBracket.Replace(Bracket, "");
+            }
             //Finding the numbers in input
-            String[] seperator = { "M", "P", "T", "D" };
+            String[] seperator = { "+", "-", "*", "/" };
             int count = 50;
-            String[] strlist = inputMod.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
+            String[] strlist = input.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
             foreach (String s in strlist)
             {
                 Numbs.Add(s);
             }
-
-            //Finding the operations in input
-            for (int i = 0; i < inputMod.Length; i++)
+            //Getting rid of brackets
+            if(bracketCheck == true)
             {
-                char Position = inputMod[i];
-                if (Position == 'P') { realOperators.Add("+"); }
-                if (Position == 'M') { realOperators.Add("-"); }
-                if (Position == 'T') { realOperators.Add("*"); }
-                if (Position == 'D') { realOperators.Add("/"); }
+                for (int p = 0; p < Numbs.Count; p++)
+                {
+                    partialResult = 0;
+                    if (Numbs[p].Contains("("))
+                    {
+                        List<string> realOperatorsInBrackets = new List<string>();
+                        List<string> NumbsInBrackets = new List<string>();
+                        List<float> NumbFloatsInBrackets = new List<float>();
+                        
+                        Numbs[p] = Numbs[p].Replace("(", "").Replace(")", "");
+                        String[] seperatorInBrackets = { "p", "m", "t", "d" };
+                        count = 50;
+                        String[] strlistBracket = Numbs[p].Split(seperatorInBrackets, count, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (String t in strlistBracket)
+                        {
+                            NumbsInBrackets.Add(t);
+                        }
+                        for (int j = 0; j < Numbs[p].Length; j++)
+                        {
+                            char Position = Numbs[p][j];
+                            if (Position == 'p') { realOperatorsInBrackets.Add("+"); }
+                            if (Position == 'm') { realOperatorsInBrackets.Add("-"); }
+                            if (Position == 't') { realOperatorsInBrackets.Add("*"); }
+                            if (Position == 'd') { realOperatorsInBrackets.Add("/"); }
+                        }
+                        NumbFloatsInBrackets = NumbsInBrackets.Select(float.Parse).ToList();
+                        while (realOperatorsInBrackets.Count != 0)
+                        {
+                            for (int i = 0; i < realOperatorsInBrackets.Count; i++)
+                            {
+                                if (realOperatorsInBrackets[i] == "*") { partialResult = NumbFloatsInBrackets[i] * NumbFloatsInBrackets[i + 1]; }
+                                if (realOperatorsInBrackets[i] == "/") { partialResult = NumbFloatsInBrackets[i] / NumbFloatsInBrackets[i + 1]; }
+                                if (realOperatorsInBrackets[i] == "+") { partialResult = NumbFloatsInBrackets[i] + NumbFloatsInBrackets[i + 1]; }
+                                if (realOperatorsInBrackets[i] == "-") { partialResult = NumbFloatsInBrackets[i] - NumbFloatsInBrackets[i + 1]; }
+                                realOperatorsInBrackets.Remove(realOperatorsInBrackets[i]);
+                                NumbFloatsInBrackets.Remove(NumbFloatsInBrackets[i + 1]);
+                                NumbFloatsInBrackets.Remove(NumbFloatsInBrackets[i]);
+                                NumbFloatsInBrackets.Insert(i, partialResult);
+                            }
+                        }
+                        Numbs[p] = Numbs[p].Replace(Numbs[p], NumbFloatsInBrackets[0].ToString());
+                    }
+                }
             }
-            //Calculating multiplication/division, Line 56 first index + 1 to not change index of first position
+            //Finding the operations in input 
+            for (int i = 0; i < input.Length; i++)
+            {
+                char Position = input[i];
+                if (Position == '+') { realOperators.Add("+"); }
+                if (Position == '-') { realOperators.Add("-"); }
+                if (Position == '*') { realOperators.Add("*"); }
+                if (Position == '/') { realOperators.Add("/"); }
+            }
+ 
+            //Calculating, Line 128 first index + 1 to not change index of first position
             NumbFloats = Numbs.Select(float.Parse).ToList();
             while (realOperators.Count != 0)
             {
                 for (int i = 0; i < realOperators.Count; i++)
                 {
-                    if (realOperators[i] == "*")
-                    {
-                        partialResult = NumbFloats[i] * NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                        //result += partialResult; Mam pocit ze tohle bude potreba
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "/")
-                    {
-                        partialResult = NumbFloats[i] / NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "+")
-                    {
-
-                        partialResult = NumbFloats[i] + NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "-")
-                    {
-                        partialResult = NumbFloats[i] - NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                    }
+                    if (realOperators[i] == "*") { partialResult = NumbFloats[i] * NumbFloats[i + 1]; }
+                    if (realOperators[i] == "/") { partialResult = NumbFloats[i] / NumbFloats[i + 1]; }
+                    if (realOperators[i] == "+") { partialResult = NumbFloats[i] + NumbFloats[i + 1]; }
+                    if (realOperators[i] == "-") { partialResult = NumbFloats[i] - NumbFloats[i + 1]; }
+                    realOperators.Remove(realOperators[i]);
+                    NumbFloats.Remove(NumbFloats[i + 1]);
+                    NumbFloats.Remove(NumbFloats[i]);
+                    NumbFloats.Insert(i, partialResult);
                 }
             }
-            //Console.WriteLine(NumbFloats.Count);
             NumbFloats.ForEach(Console.WriteLine);
-            //realOperators.ForEach(Console.WriteLine);
             Console.ReadKey();
 
         }
