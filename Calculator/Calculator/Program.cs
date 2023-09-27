bf0bbe868dd4ab169b178a5cf8b9b02bd3812a39
VelkyCajicek <peichl294@gmail.com>
2023-09-27 11:31:25 +0200

    Message

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -20,33 +20,48 @@ namespace Calculator
             List<string> realOperators = new List<string>();
             List<string> Numbs = new List<string>();
             List<float> NumbFloats = new List<float>();
-            float result = 0;
-            float partialResult = 0;
-
+            float partialResult = 0; int firstStringPosition = 0; int secondStringPosition = 0;
 
             string input = Console.ReadLine();
 
             //Making sure there is no BS
-            string inputMod = input.Replace('+', 'P').Replace('-', 'M').Replace('*', 'T').Replace('/', 'D');
             if (input.Last() == '=') { input = input.Remove(input.Length - 1); }
 
+            //Zavorky
+
+            string inputModBracket = input;
+            while (inputModBracket.Contains('(') == true)
+            {
+                for (int i = 0; i < inputModBracket.Length; i++)
+                {
+                    if (inputModBracket[i] == '(') { firstStringPosition = i; }
+                    if (inputModBracket[i] == ')') { secondStringPosition = i; }
+                }
+                string Bracket = inputModBracket.Substring(firstStringPosition, secondStringPosition - firstStringPosition + 1);
+                string BracketMod = Bracket.Replace('+', 'p').Replace('-', 'm').Replace('*', 't').Replace('/', 'd');
+                input = input.Replace(Bracket, BracketMod);
+                inputModBracket = inputModBracket.Replace(Bracket, "");
+            }
+            //Console.WriteLine(inputMod);
+            
             //Finding the numbers in input
-            String[] seperator = { "M", "P", "T", "D" };
+            String[] seperator = { "+", "-", "*", "/" };
             int count = 50;
-            String[] strlist = inputMod.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
+            String[] strlist = input.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
             foreach (String s in strlist)
             {
                 Numbs.Add(s);
             }
-
-            //Finding the operations in input
-            for (int i = 0; i < inputMod.Length; i++)
+            Numbs.ForEach(Console.WriteLine);
+            //Finding the operations in input 
+            /*
+            for (int i = 0; i < input.Length; i++)
             {
-                char Position = inputMod[i];
-                if (Position == 'P') { realOperators.Add("+"); }
-                if (Position == 'M') { realOperators.Add("-"); }
-                if (Position == 'T') { realOperators.Add("*"); }
-                if (Position == 'D') { realOperators.Add("/"); }
+                char Position = input[i];
+                if (Position == '+') { realOperators.Add("+"); }
+                if (Position == '-') { realOperators.Add("-"); }
+                if (Position == '*') { realOperators.Add("*"); }
+                if (Position == '/') { realOperators.Add("/"); }
             }
             //Calculating multiplication/division, Line 56 first index + 1 to not change index of first position
             NumbFloats = Numbs.Select(float.Parse).ToList();
@@ -54,56 +69,22 @@ namespace Calculator
             {
                 for (int i = 0; i < realOperators.Count; i++)
                 {
-                    if (realOperators[i] == "*")
-                    {
-                        partialResult = NumbFloats[i] * NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                        //result += partialResult; Mam pocit ze tohle bude potreba
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "/")
-                    {
-                        partialResult = NumbFloats[i] / NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "+")
-                    {
-
-                        partialResult = NumbFloats[i] + NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "-")
-                    {
-                        partialResult = NumbFloats[i] - NumbFloats[i + 1];
-                        realOperators.Remove(realOperators[i]);
-                        NumbFloats.Remove(NumbFloats[i + 1]);
-                        NumbFloats.Remove(NumbFloats[i]);
-                        NumbFloats.Insert(i, partialResult);
-                    }
+                    if (realOperators[i] == "*") { partialResult = NumbFloats[i] * NumbFloats[i + 1]; }
+                    if (realOperators[i] == "/") { partialResult = NumbFloats[i] / NumbFloats[i + 1]; }
+                    if (realOperators[i] == "+") { partialResult = NumbFloats[i] + NumbFloats[i + 1]; }
+                    if (realOperators[i] == "-") { partialResult = NumbFloats[i] - NumbFloats[i + 1]; }
+                    realOperators.Remove(realOperators[i]);
+                    NumbFloats.Remove(NumbFloats[i + 1]);
+                    NumbFloats.Remove(NumbFloats[i]);
+                    NumbFloats.Insert(i, partialResult);
                 }
             }
             //Console.WriteLine(NumbFloats.Count);
             NumbFloats.ForEach(Console.WriteLine);
             //realOperators.ForEach(Console.WriteLine);
+            */
             Console.ReadKey();
-
+            
         }
     }
 }
