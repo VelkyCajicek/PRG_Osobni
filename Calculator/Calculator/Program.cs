59cd6294aa71370362e46c9539f9f71e17888612
unknown <peichl294@gmail.com>
2023-10-05 21:06:17 +0200

    Calculator Mk2

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
@@ -15,237 +16,151 @@ namespace Calculator
 {
     internal class Program
     {
-        static void Calculation(int i, float partialResult, List<string> realOperators, List<float> NumbFloats)
-        {
-            realOperators.Remove(realOperators[i]);
-            NumbFloats.Remove(NumbFloats[i + 1]);
-            NumbFloats.Remove(NumbFloats[i]);
-            NumbFloats.Insert(i, partialResult);
-        }
-        static void CalculationBracket(int i, float partialResult, List<string> realOperatorsInBrackets, List<float> NumbFloatsInBrackets)
-        {
-            realOperatorsInBrackets.Remove(realOperatorsInBrackets[i]);
-            NumbFloatsInBrackets.Remove(NumbFloatsInBrackets[i + 1]);
-            NumbFloatsInBrackets.Remove(NumbFloatsInBrackets[i]);
-            NumbFloatsInBrackets.Insert(i, partialResult);
-        }
-        static void Exponentiation(List<string> Numbs)
-        {
-            for (int i = 0; i < Numbs.Count; i++)
-            {
-                if (Numbs[i].Contains("^"))
-                {
-                    string correctString = Numbs[i];
-                    string x = correctString.Split('^')[0];
-                    string n = correctString.Replace(x, "").Replace("^", "");
-                    float xFloat = float.Parse(x); float nFloat = float.Parse(n);
-                    float r = (float)Math.Pow(xFloat, nFloat);
-                    Numbs[i] = Numbs[i].Replace(Numbs[i], r.ToString());
-                }
-                else { }
-            }
-        }
-        static void Result(List<float> NumbFloats, List<string> realOperators, string input)
-        {
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (input[i] == '+') { realOperators.Add("+"); }
-                if (input[i] == '-') { realOperators.Add("-"); }
-                if (input[i] == '*') { realOperators.Add("*"); }
-                if (input[i] == '/') { realOperators.Add("/"); }
-            }
-            float partialResult = 0;
-            while (realOperators.Count != 0)
-            {
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "*")
-                    {
-                        partialResult = NumbFloats[i] * NumbFloats[i + 1];
-                        Calculation(i, partialResult, realOperators, NumbFloats);
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "/")
-                    {
-                        partialResult = NumbFloats[i] / NumbFloats[i + 1];
-                        Calculation(i, partialResult, realOperators, NumbFloats);
-                    }
-                }
-                for (int i = 0; i < realOperators.Count; i++)
-                {
-                    if (realOperators[i] == "+") { partialResult = NumbFloats[i] + NumbFloats[i + 1]; }
-                    if (realOperators[i] == "-") { partialResult = NumbFloats[i] - NumbFloats[i + 1]; }
-                    Calculation(i, partialResult, realOperators, NumbFloats);
-                }
-            }
-            NumbFloats.ForEach(Console.WriteLine);
-        }
-        static void BracketResult(List<string> Numbs)
-        {
-            for (int p = 0; p < Numbs.Count; p++)
-            {
-                float partialResult = 0;
-                if (Numbs[p].Contains("("))
-                {
-                    List<string> realOperatorsInBrackets = new List<string>();
-                    List<string> NumbsInBrackets = new List<string>();
-                    List<float> NumbFloatsInBrackets = new List<float>();
-
-                    Numbs[p] = Numbs[p].Replace("(", "").Replace(")", "");
-                    String[] seperatorInBrackets = { "p", "m", "t", "d" };
-                    int count = 50;
-                    String[] strlistBracket = Numbs[p].Split(seperatorInBrackets, count, StringSplitOptions.RemoveEmptyEntries);
-                    foreach (String t in strlistBracket)
-                    {
-                        NumbsInBrackets.Add(t);
-                    }
-                    for (int j = 0; j < Numbs[p].Length; j++)
-                    {
-                        if (Numbs[p][j] == 'p') { realOperatorsInBrackets.Add("+"); }
-                        if (Numbs[p][j] == 'm') { realOperatorsInBrackets.Add("-"); }
-                        if (Numbs[p][j] == 't') { realOperatorsInBrackets.Add("*"); }
-                        if (Numbs[p][j] == 'd') { realOperatorsInBrackets.Add("/"); }
-                    }
-                    NumbFloatsInBrackets = NumbsInBrackets.Select(float.Parse).ToList();
-                    while (realOperatorsInBrackets.Count != 0)
-                    {
-                        for (int i = 0; i < realOperatorsInBrackets.Count; i++)
-                        {
-                            if (realOperatorsInBrackets[i] == "*")
-                            {
-                                partialResult = NumbFloatsInBrackets[i] * NumbFloatsInBrackets[i + 1];
-                                CalculationBracket(i, partialResult, realOperatorsInBrackets, NumbFloatsInBrackets);
-                            }
-                        }
-                        for (int i = 0; i < realOperatorsInBrackets.Count; i++)
-                        {
-                            if (realOperatorsInBrackets[i] == "/")
-                            {
-                                partialResult = NumbFloatsInBrackets[i] / NumbFloatsInBrackets[i + 1];
-                                CalculationBracket(i, partialResult, realOperatorsInBrackets, NumbFloatsInBrackets);
-                            }
-                        }
-                        for (int i = 0; i < realOperatorsInBrackets.Count; i++)
-                        {
-                            if (realOperatorsInBrackets[i] == "+") { partialResult = NumbFloatsInBrackets[i] + NumbFloatsInBrackets[i + 1]; }
-                            if (realOperatorsInBrackets[i] == "-") { partialResult = NumbFloatsInBrackets[i] - NumbFloatsInBrackets[i + 1]; }
-                            CalculationBracket(i, partialResult, realOperatorsInBrackets, NumbFloatsInBrackets);
-                        }
-                    }
-                    Numbs[p] = Numbs[p].Replace(Numbs[p], NumbFloatsInBrackets[0].ToString());
-                }
-            }
-        }
         static void Main(string[] args)
         {
-            bool test = true;
+            Explanation(); //This is just so the user knows what the program can do
+            List<string> variableName = new List<string>();
+            List<string> variableValue = new List<string>();
             List<string> realOperators = new List<string>();
             List<string> Numbs = new List<string>();
-            List<float> NumbFloats = new List<float>();
-            List<string> jmenoPromenne = new List<string>();
-            List<string> hodnotaPromenne = new List<string>();
-            string pattern = "[A-Za-z]=[0-9]+"; Regex promenna = new Regex(pattern); string input = "";
+            bool test = true;
             while (test == true)
             {
-                int firstStringPosition = 0; int secondStringPosition = 0; bool bracketCheck = false; bool check1 = false; bool check2 = false;
-                bool exponentiationCheck = false; Numbs.Clear(); NumbFloats.Clear();
-                while (check1 == false && check2 == false)
+                string input = ""; bool check1 = false; string inputFinal = ""; realOperators.Clear(); Numbs.Clear(); string inputMod1 = "";
+                while (check1 == false)
                 {
                     int counter = 0;
-                    while (true)
+                    while (true) // This is for when a user defines a variable
                     {
-                        input = Console.ReadLine();
-                        input = input.Replace(" ", "");
-                        if (promenna.IsMatch(input) == true)
+                        input = Console.ReadLine().Replace(" ", "");
+                        if (Regex.IsMatch(input, "[A-Za-z]=[0-9]+") == true)
                         {
-                            string jmenoPromenneHodnota = input[0].ToString();
-                            string hodnotaPromenneHodnota = input.Remove(0, 2);
-                            if (jmenoPromenne.Contains(input[0].ToString()) == true)
+                            if (variableName.Contains(input[0].ToString()) == true) //When a variable is given a different value
                             {
-                                int index = jmenoPromenne.IndexOf(jmenoPromenneHodnota);
-                                string previousString = hodnotaPromenne[index];
-                                hodnotaPromenne[index] = hodnotaPromenneHodnota;
-                                Console.WriteLine("'{0}' now has the value {1}, (previous value {2})", jmenoPromenne[index], hodnotaPromenneHodnota, previousString);
+                                int index = variableName.IndexOf(input[0].ToString());
+                                string previousString = variableValue[index];
+                                variableValue[index] = input.Remove(0, 2);
+                                Console.WriteLine("'{0}' now has the value {1}, (previous value {2})", variableName[index], input.Remove(0, 2), previousString);
                             }
-                            else
+                            else //When a new variable is added
                             {
-                                jmenoPromenne.Add(jmenoPromenneHodnota);
-                                hodnotaPromenne.Add(hodnotaPromenneHodnota);
-                                Console.WriteLine("'{0}' now has the value {1}", jmenoPromenne[counter], hodnotaPromenne[counter]);
+                                variableName.Add(input[0].ToString());
+                                variableValue.Add(input.Remove(0, 2));
+                                Console.WriteLine("'{0}' now has the value {1}", variableName[counter], variableValue[counter]);
+                                counter++;
                             }
-                            counter++;
                         }
                         else { break; }
                     }
-                    while (check1 == false && check2 == false)
+                    while (check1 == false) //Getting rid of trigoniometric functions so input can be checked
                     {
-                        check1 = false; check2 = false; bracketCheck = false; int counterBracket = 0; bool breakCheck = false;
-                        if (input.Contains("pi") == true) { input = input.Replace("pi", Math.PI.ToString()); }
-                        for (int i = 0; i < input.Length; i++)
+                        int counterBracket = 0; bool breakCheck = false;
+                        input = replacingVariables(variableName, input, variableValue);
+                        inputMod1 = input;
+                        if (input.Contains("pi")) { input = input.Replace("pi", Math.PI.ToString()); }
+                        if (input.Contains("sin(")) { input = input.Replace("sin", ""); }
+                        if (input.Contains("cos(")) { input = input.Replace("cos", ""); }
+                        if (input.Contains("tg(")) { input = input.Replace("tg", ""); }
+                        for (int i = 0; i < input.Length; i++) // Simple check to make sure input hasnt got any undefined variables
                         {
                             if (Regex.IsMatch(input[i].ToString(), @"^[a-zA-Z]+$") == true)
                             {
-                                if (jmenoPromenne.Contains(input[i].ToString()) == false) { Console.WriteLine("Syntax error, '{0}' not defined", input[i]); breakCheck = true; }
+                                if (variableName.Contains(input[i].ToString()) == false) { Console.WriteLine("Syntax error, '{0}' not defined", input[i]); breakCheck = true; }
                                 else { counterBracket++; }
                             }
                             else { counterBracket++; }
                             if (counterBracket == input.Length) { check1 = true; }
                         }
                         if (breakCheck == true) { break; }
-                        if (input.Contains('(') == true)
-                        {
-                            bracketCheck = true;
-                            if (input.Contains(")") == false) { Console.WriteLine("Syntax error, missing ')'"); break; }
-                            else { check2 = true; }
-                        }
                     }
                 }
-                if (input.Contains('^') == true) { exponentiationCheck = true; }
-                for (int i = 0; i < jmenoPromenne.Count; i++) // Replacing the variables
+                input = inputMod1;
+                for (int i = 0; i < input.Length; i++) //Adding the operators in the input to appropriate list
                 {
-                    if (input.Contains(jmenoPromenne[i]))
+                    if (input[i] == '+') { realOperators.Add("+"); }
+                    if (input[i] == '-') { realOperators.Add("-"); }
+                    if (input[i] == '*') { realOperators.Add("*"); }
+                    if (input[i] == '/') { realOperators.Add("/"); }
+                }
+                String[] seperator = { "+", "-", "*", "/" }; // Spliting the string to find all numbers/parentheses
+                String[] strlist = input.Split(seperator, 50, StringSplitOptions.RemoveEmptyEntries);
+                foreach (String s in strlist) { Numbs.Add(s); }
+                for (int i = 0; i < Numbs.Count; i++)
+                {
+                    if (Numbs[i].Contains("^")) // Dealing with exponentials
                     {
-                        input = input.Replace(jmenoPromenne[i], hodnotaPromenne[i]);
-                    }
+                        string[] parts = Numbs[i].Split('^');
+                        string x = parts[0]; string n = parts[1];
+                        float xFloat = float.Parse(x); float nFloat = float.Parse(n);
+                        float r = (float)Math.Pow(xFloat, nFloat);
+                        Numbs[i] = Numbs[i].Replace(Numbs[i], r.ToString());
+                    }//Dealing with trigoniometric functions
+                    if (Numbs[i].Contains("sin(")) { Sin(Numbs, i); }
+                    if (Numbs[i].Contains("cos(")) { Cos(Numbs, i); }
+                    if (Numbs[i].Contains("tg(")) { Tg(Numbs, i); }
                 }
-                if (bracketCheck == true) //Modifying the string so it can be calculated 
+                for (int i = 0; i < Numbs.Count; i++) //Reconstructing string so it can go into datatable without any error
                 {
-                    string inputModBracket = input;
-                    while (inputModBracket.Contains('(') == true)
-                    {
-                        for (int i = 0; i < inputModBracket.Length; i++)
-                        {
-                            if (inputModBracket[i] == '(') { firstStringPosition = i; }
-                            if (inputModBracket[i] == ')') { secondStringPosition = i; }
-                        }
-                        string Bracket = inputModBracket.Substring(firstStringPosition, secondStringPosition - firstStringPosition + 1);
-                        string BracketMod = Bracket.Replace('+', 'p').Replace('-', 'm').Replace('*', 't').Replace('/', 'd');
-                        input = input.Replace(Bracket, BracketMod);
-                        inputModBracket = inputModBracket.Replace(Bracket, "");
-                    }
+                    try { inputFinal += Numbs[i]; inputFinal += realOperators[i]; }
+                    catch (ArgumentOutOfRangeException) { break; }
                 }
-                //Finding the numbers in input
-                String[] seperator = { "+", "-", "*", "/" };
-                int count = 50;
-                String[] strlist = input.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
-                foreach (String s in strlist)
+                string inputMod = inputFinal;
+                try
                 {
-                    Numbs.Add(s);
-                }
-                if (exponentiationCheck == true) // Calculating the exponents
-                {
-                    Exponentiation(Numbs);
-                }
-                if (bracketCheck == true) //Getting rid of brackets
-                {
-                    BracketResult(Numbs);
-                }
-                NumbFloats = Numbs.Select(float.Parse).ToList();
-                Result(NumbFloats, realOperators, input);
+                    double result = Calculation(inputMod);
+                    Console.WriteLine(result);
+                } //In case of multiple operators after each other (e.g. 4*-4) or missing parentheses ( 4+(4+4 )
+                catch (SyntaxErrorException) { Console.WriteLine("Syntax error, Invalid expression"); }
             }
             Console.ReadKey();
+        }
+        static void Explanation()
+        {
+            Console.WriteLine("This calculator can calculate:");
+            Console.WriteLine("- Basic operations (+,-,*,/)");
+            Console.WriteLine("- Brackets and exponentials (to get square root enter 0.5 to exponent)");
+            Console.WriteLine("- Trigonimetric functions (sin, cos, tg)");
+            Console.WriteLine("- User defined variables (e.g. x = 4)\n");
+        }
+        static string replacingVariables(List<string> variableName, string input, List<string> variableValue) //Replacing the variables with their corresponding value
+        {
+            for (int i = 0; i < variableName.Count; i++)
+            {
+                if (input.Contains(variableName[i]))
+                {
+                    input = input.Replace(variableName[i], variableValue[i]);
+                }
+            }
+            return (input);
+        }
+        static double Calculation(string inputMod) // Only lines of code that were inspired (written) by ChatGPT, uses the columns in the datatable to calculate simple mathematical operations
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("input", typeof(string), inputMod);
+            DataRow row = dt.NewRow();
+            dt.Rows.Add(row);
+            return (double.Parse((string)row["input"]));
+        }
+        static void Sin(List<string> Numbs, int i) // The following 3 methods just calculate the 3 implemented trigoniometric functions
+        {
+            Numbs[i] = Numbs[i].Replace("sin", "");
+            string inputMod = Numbs[i];
+            string result = Math.Sin(Calculation(inputMod) * (Math.PI / 180)).ToString();
+            Numbs[i] = Numbs[i].Replace(inputMod, result);
+        }
+        static void Cos(List<string> Numbs, int i)
+        {
+            Numbs[i] = Numbs[i].Replace("cos", "");
+            string inputMod = Numbs[i];
+            string result = Math.Cos(Calculation(inputMod) * (Math.PI / 180)).ToString();
+            Numbs[i] = Numbs[i].Replace(inputMod, result);
+        }
+        static void Tg(List<string> Numbs, int i)
+        {
+            Numbs[i] = Numbs[i].Replace("tg", "");
+            string inputMod = Numbs[i];
+            string result = Math.Tan(Calculation(inputMod) * (Math.PI / 180)).ToString();
+            Numbs[i] = Numbs[i].Replace(inputMod, result);
         }
     }
 }
