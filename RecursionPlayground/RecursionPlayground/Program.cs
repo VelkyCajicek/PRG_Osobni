f8a103f68f8481366c16f173e1a610a245f034ae
VelkyCajicek <peichl294@gmail.com>
2023-10-18 10:42:33 +0200

    School work

--- RecursionPlayground/RecursionPlayground/Program.cs
+++ RecursionPlayground/RecursionPlayground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -11,25 +12,35 @@ namespace RecursionPlayground
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
-            int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
-            int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
-            Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            List<string> URLS = new List<string>();
+            //int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
+            //int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
+            //int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
+            //Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            string input = "Programming";
+            Files(URLS);
+            URLS.ForEach(Console.WriteLine);
             Console.ReadKey();
         }
 
         static int Factorial(int n)
         {
-            int resultF = 1;
-            for (int i = 1; i < n+1; i++) { resultF *= i; }
-            return resultF;
+            if (n == 1) { return 1; }
+            else { return n * Factorial(n - 1); }
         }
         static int Fibonacci(int n)
         {
-            double Nd = double.Parse(n.ToString());
-            double resultD = (int)Math.Pow(1 + Math.Sqrt(5), Nd) / (int)(Math.Pow(2, n) * Math.Sqrt(5));
-            int resultI = (int)Math.Round(resultD);
-            return resultI;
+            if (n == 0) { return 0; }
+            if (n == 1) { return 1; }
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Files(List<string> URLS)
+        {
+            string path = @"C:\\Users\\Klaudia Peichlova\\Documents";
+            foreach(string folder in Directory.GetDirectories(path)) // Funguje
+            {
+                URLS.Add(folder);
+            }
         }
     }
 }
