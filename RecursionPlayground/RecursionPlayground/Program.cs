5984e1faead4c7ca74d7c464f9264ef89dec948b
unknown <peichl294@gmail.com>
2023-11-03 20:16:11 +0100

    Should work now

--- RecursionPlayground/RecursionPlayground/Program.cs
+++ RecursionPlayground/RecursionPlayground/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace RecursionPlayground
 {
@@ -11,25 +13,38 @@ namespace RecursionPlayground
     {
         static void Main(string[] args)
         {
+            /*
             int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
             int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
             int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
             Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            */
+            Files();
             Console.ReadKey();
         }
 
         static int Factorial(int n)
         {
-            int resultF = 1;
-            for (int i = 1; i < n+1; i++) { resultF *= i; }
-            return resultF;
+            if(n == 1) { return 1; }
+            else { return n * Factorial(n - 1); }
         }
         static int Fibonacci(int n)
         {
-            double Nd = double.Parse(n.ToString());
-            double resultD = (int)Math.Pow(1 + Math.Sqrt(5), Nd) / (int)(Math.Pow(2, n) * Math.Sqrt(5));
-            int resultI = (int)Math.Round(resultD);
-            return resultI;
+            if(n == 1) { return 1; };
+            if(n == 0) {  return 0; }
+            return Fibonacci(n - 1) + Fibonacci(n-2);
+        }
+        static void Files()
+        {
+            string path = @"E:\\Tobias";
+            foreach(string folder in Directory.GetDirectories(path))
+            {
+                Console.WriteLine(folder);
+            }
+            foreach (string file in Directory.GetFiles(path))
+            {
+                Console.WriteLine(file);
+            }
         }
     }
 }
