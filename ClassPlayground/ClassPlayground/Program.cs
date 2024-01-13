8b598e02723d57d4e696d3c0c082a34c351bd7ae
unknown <peichl294@gmail.com>
2024-01-13 22:25:15 +0100

    Chess Project Commit, Sadly doesnt work

---
+++ ClassPlayground/ClassPlayground/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RectangleCode();
+            BankCode();
+            Console.ReadKey();
+        }
+        static void RectangleCode()
+        {
+            Rectangle rect = new Rectangle();
+            rect.height = 10;
+            rect.width = 10;
+            Console.WriteLine($"Obsah je {rect.CalculateArea()}");
+
+            double ratio = rect.CalculateAspectRatio();
+            if (ratio > 1) { Console.WriteLine("Obdelnik je siroky"); }
+            if (ratio < 1) { Console.WriteLine("Obdelnik je vysoky"); }
+            if (ratio == 1) { Console.WriteLine("Je to ctverec"); }
+
+            int inputX = 5; int inputY = 5;
+            Console.WriteLine(rect.ContainsPoints(inputX, inputY));
+        }
+        static void BankCode()
+        {
+            BankAccount Account1 = new BankAccount();
+            Account1.balance = 100;
+            Account1.accountNumber = 1;
+            Account1.holderName = "Bob";
+            Account1.currency = "Kc";
+            
+            BankAccount Account2 = new BankAccount();
+            Account2.balance = 1000;
+            Account2.accountNumber = 2;
+            Account2.holderName = "Tom";
+            Account2.currency = "Kc";
+
+            int amount = 100;
+
+            Console.WriteLine($"{amount} has been deposited and the balance now stands at {Account1.Deposit(amount)}");
+            Console.WriteLine($"{amount} has been withdrawn and the balance now stands at {Account2.Withdrawel(amount)}");
+            Transfer(Account1 , Account2, amount);
+            Console.WriteLine($"{amount} has been transfered between {Account1.accountNumber} and {Account2.accountNumber}. The balances now stand at {Account1.balance} and {Account2.balance}");
+        }
+        static void Transfer(BankAccount account1, BankAccount account2, int amount)
+        {
+            account1.balance -= amount;
+            account2.balance += amount;
+        }
+    }
+}
+class Rectangle
+{
+    public int height;
+    public int width;
+
+    public Rectangle(int height, int width)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public Rectangle()
+    {
+    }
+    public int CalculateArea()
+    {
+        int area = height * width;
+        return area;
+    }
+    public double CalculateAspectRatio()
+    {
+        return width / height;
+    }
+    public bool ContainsPoints(int inputX, int inputY)
+    {
+        if((0 < inputX && inputX < height) && (0 < inputY && inputY < width)) 
+        {
+            return true;
+        }
+        else { return false; }
+    }
+}
+
+class BankAccount
+{
+    public int accountNumber;
+    public string holderName;
+    public string currency;
+    public int balance;
+    public BankAccount(int accountNumber, string holderName, string currency, int balance)
+    {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.currency = currency;
+        this.balance = balance;
+    }
+    public BankAccount()
+    {
+    }
+    public int Deposit(int amount)
+    {
+        return balance + amount;
+    }
+    public int Withdrawel(int amount)
+    {
+        return balance - amount;
+    }
+}
+class Student
+{
+    public int year;
+    public int id;
+    public Dictionary<string, string> Subjects;
+    public string name;
+}
--- GameProject/GameProject/Program.cs
+++ GameProject/GameProject/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Security.Permissions;
 using System.Text;
@@ -12,149 +14,343 @@ namespace GameProject
     {
         static void Main(string[] args)
         {
-            /*
-            string[,] board = {
-                {"BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR" },
-                {"BP" ,"BP","BP","BP","BP","BP","BP","BP" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"WP","WP","WP","WP","WP","WP","WP","WP", },
-                {"WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR" }
-            };
-            */
-            string[,] board = {
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","BB","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"E","E","E","E","E","E","E","E" },
-                {"WR","E","E","E","E","E","E","E" },
-            };
             Dictionary<string, int> positions = new Dictionary<string, int>()
             {
                 { "1", 7 }, {"2", 6 }, {"3",5}, {"4", 4}, {"5", 3}, {"6", 2}, {"7", 1}, {"8", 0},
                 { "H", 7 }, {"G", 6 }, {"F",5}, {"E", 4}, {"D", 3}, {"C", 2}, {"B", 1}, {"A", 0}
             };
-            string testInput = "A1-A6";
-            
-            int[] firstPosition = new int[2]; int[] secondPosition = new int[2];
-            firstPosition[0] = positions[testInput[1].ToString()]; firstPosition[1] = positions[testInput[0].ToString()];
-            secondPosition[0] = positions[testInput[4].ToString()]; secondPosition[1] = positions[testInput[3].ToString()];
+            string userInput;
+            string[,] board = new string[8,8]; fillBoard(board);
+            //userInput = "A1H8";
+            // Setting up the board
+            // White pieces
+            Rook WhiteRook1 = new Rook(7, 7, 'W', board);
+            Rook WhiteRook2 = new Rook(7, 0, 'W', board);
+            Knight WhiteKnight1 = new Knight(7, 6, 'W', board);
+            Knight WhiteKnight2 = new Knight(7, 1, 'W', board);
+            Bishop WhiteBishop1 = new Bishop(7, 2, 'W' ,board);
+            Bishop WhiteBishop2 = new Bishop(7, 5, 'W', board);
+            King WhiteKing = new King(7, 3, 'W', board);
+            Queen WhiteQueen = new Queen(7, 4, 'W', board);
+            // Black pieces
+            Rook BlackRook1 = new Rook(0, 7, 'B', board);
+            Rook BlackRook2 = new Rook(0, 0, 'B', board);
+            Knight BlackKnight1 = new Knight(0, 6, 'B', board);
+            Knight BlackKnight2 = new Knight(0, 1, 'B', board);
+            Bishop BlackBishop1 = new Bishop(0, 2, 'B', board);
+            Bishop BlackBishop2 = new Bishop(0, 5, 'B', board);
+            King BlackKing = new King(0, 3, 'B', board);
+            Queen BlackQueen = new Queen(0, 4, 'B', board);
+            //Pawns
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                Pawn WhitePawn = new Pawn(6, i, 'W', board);
+                Pawn BlackPawn = new Pawn(1, i, 'B', board);
+            }
+            //DisplayBoard(board);
+            //Game
+            int turn = 0;
+            // Validate user input
+            while (true)
+            {
+                while (true)
+                {
+                    Console.WriteLine("This is the current board:");
+                    DisplayBoard(board);
+                    Console.WriteLine("Enter your move");
+                    userInput = Console.ReadLine();
+                    if (validateUserInput(userInput, positions) == true) { break; }
+                    else { Console.WriteLine("Position doesnt exist"); }
+                }
+                while (true)
+                {
+                    // First position coordinates
+                    int x = positions[userInput[0].ToString()]; int y = positions[userInput[1].ToString()];
+                    // Second position coordinates
+                    int a = positions[userInput[2].ToString()]; int b = positions[userInput[3].ToString()];
+                    // Info about current piece
+                    string pieceType = board[x, y][1].ToString(); string pieceColor = board[x, y][0].ToString();
 
-            //var firstPosition = Tuple.Create(positions[testInput[1].ToString()], positions[testInput[0].ToString()]);
-            //var secondPosition = Tuple.Create(positions[testInput[4].ToString()], positions[testInput[3].ToString()]);
-            
-            if(getPieceInfo(board, firstPosition, secondPosition) == true) { board = Move(board, firstPosition, secondPosition); displayBoard(board); }
-            else { Console.WriteLine("Move not possible"); }               
+                    Console.WriteLine($"{x} {y} {a} {b}");
+                    Console.WriteLine(board[x, y]);
+
+                    if (LegalMove(board,turn, pieceColor, pieceType, x, y, a, b) == false) { Console.WriteLine("Not a legal move"); break; }
+                    else
+                    {
+                        ChessPieces.Move(x, y, a, b, board); turn++; break;
+                    }
+                }
+            }
             Console.ReadKey();
         }
-        static int Max(int num1,  int num2)
+        static bool validateUserInput(string userInput, Dictionary<string, int> positions)
         {
-            if(num1 > num2)
+            if(userInput.Length != 4) return false;
+            for(int i = 0; i < userInput.Length; i++)
             {
-                return num1;
+                if (positions.ContainsKey(userInput[i].ToString()) == false){ return false; }
             }
-            else
-            {
-                return num2;
-            }
-        } 
-        static int Min(int num1, int num2) 
-        {
-            if( num1 < num2)
-            {
-                return num1;
-            }
-            else
-            {
-                return num2;
-            }
+            return true;
         }
-        static bool Rook(string[,] board, dynamic firstPosition, dynamic secondPosition)
-        {
-            if (firstPosition[0] == secondPosition[0])
-            {
-                int axis = firstPosition[0];
-                int end_point = Max(firstPosition[1], secondPosition[1]);
-                int start_point = Min(firstPosition[1], secondPosition[1]);
-                for (int i = start_point+1; i < end_point; i++)
-                {
-                    if (board[axis, i] != "E") { return false; }
-                }
-                return true;
-            }
-            if (firstPosition[1] == secondPosition[1])
-            {
-                int axis = firstPosition[1];
-                int end_point = Max(firstPosition[0], secondPosition[0]);
-                int start_point = Min(firstPosition[0], secondPosition[0]);
-                for (int i = start_point+1; i < end_point; i++)
-                {
-                    if (board[i, axis] != "E") { return false; }
-                }
-                return true;
-            }
-            else { return false; }
-        }
-        static bool Bishop(string[,] board, dynamic firstPosition, dynamic secondPosition)
-        {
-            if (firstPosition[0] < secondPosition[0] && firstPosition[1] < secondPosition[1])
-            {
-                for (int i = firstPosition[0]; i < secondPosition[0]; i++)
-                {
-                    if (board[i,i] != "E") { return false; }
-                }
-                return true;
-            }
-            return false;
-        }
-        
-        static bool King(string[,] board, dynamic firstPosition, dynamic secondPosition)
-        {
-            bool check1 = false;
-            bool check2 = false;
-            if ((firstPosition[0] + 1 == secondPosition[0]) || (firstPosition[0] - 1 == secondPosition[0]))
-            {
-                check1 = true;
-            }
-            else { return fa}
-            return false;
-        }
-
-        static bool getPieceInfo(string[,] board, dynamic firstPosition, dynamic secondPosition)
-        {
-            string piece = board[firstPosition[0], firstPosition[1]];
-            switch (piece[1])
-            {
-                case 'P': return false;
-                case 'N': return false;
-                case 'B': return false;
-                case 'R': if (Rook(board, firstPosition, secondPosition) == true) { return true; } return false;
-                default: return false;
-            }
-        }
-        static string[,] Move(string[,] board, dynamic firstPosition, dynamic secondPosition)
-        {
-            string temp = board[secondPosition[0], secondPosition[1]];
-            board[secondPosition[0], secondPosition[1]] = board[firstPosition[0], firstPosition[1]];
-            if(temp != "E") { board[firstPosition[0], firstPosition[1]] = "E"; }
-            else { board[firstPosition[0], firstPosition[1]] = temp; }
-            return board;
-        }
-        static void displayBoard(string[,] board)
+        static void DisplayBoard(string[,] board)
         {
             for (int i = 0; i < board.GetLength(0); i++)
             {
                 for (int j = 0; j < board.GetLength(1); j++)
                 {
-                    Console.Write(board[i, j] + "\t");
+                    Console.Write(" " + board[i, j] + " ");
                 }
-                Console.WriteLine();
+                Console.Write("\n");
             }
+        }
+        static string[,] fillBoard(string[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = "00";
+                }
+            }
+            return board;
+        }
+        static int Max(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+        static int Min(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+        static bool RookMove(int x, int y, int a, int b, string[,] board)
+        {
+            if (x == a)
+            {
+                int axis = x;
+                int end_point = Max(y, b);
+                int start_point = Min(y, b);
+                for (int i = start_point + 1; i < end_point; i++)
+                {
+                    if (board[axis, i] != "00") { return false; }
+                }
+                return true;
+            }
+            if (y == b)
+            {
+                int axis = y;
+                int end_point = Max(x, a);
+                int start_point = Min(x, a);
+                for (int i = start_point + 1; i < end_point; i++)
+                {
+                    if (board[i, axis] != "00") { return false; }
+                }
+                return true;
+            }
+            else { return false; }
+        }
+        static bool BishopMove(int x, int y, int a, int b, string[,] board)
+        {
+            List<string> path = new List<string>();
+            bool check1 = false;
+            bool check2 = false;
+
+            if ((a - x) >= 0) { check1 = true; }
+            if ((b - y) >= 0) { check2 = true; }
+
+            if (Math.Abs((x - a) / (y - b)) != 1) { return false; }
+            if (check1 && check2)
+            {
+                for (int i = 0; i < (a - x - 1); i++) { path.Add(board[x + 1 + i, y + 1 + i]); }
+            }
+            if (check1 && check2 == false)
+            {
+                for (int i = 0; i < (a - x - 1); i++) { path.Add(board[x + 1 + i, y + 1 - i]); }
+            }
+            if (check1 == false && check2)
+            {
+                for (int i = 0; i < (a - x - 1); i++) { path.Add(board[x + 1 - i, y + 1 + i]); }
+            }
+            if (check2 == false && check1 == false)
+            {
+                for (int i = 0; i < (a - x - 1); i++) { path.Add(board[x + 1 - i, y + 1 - i]); }
+            }
+            for (int i = 0; i < path.Count(); i++)
+            {
+                if (path[i] != "00") { return false; }
+            }
+            return true;
+        }
+        static bool KingMove(int x, int y, int a, int b, string[,] board)
+        {
+            if (Math.Abs(x - a) > 1) { return false; }
+            if (Math.Abs(y - b) > 1) { return false; }
+            return true;
+        }
+        static bool QueenMove(int x, int y, int a, int b, string[,] board)
+        {
+            List<string> path = new List<string>();
+            bool check1 = false;
+            bool check2 = false;
+
+            if ((a - x) >= 0) { check1 = true; }
+            if ((b - y) >= 0) { check2 = true; }
+
+            if ((Math.Abs((x - a) / (y - b)) != 1) || ((x - a) != 0) || ((y - b) != 0))
+            {
+                return false;
+            }
+            if (x - a == 0)
+            {
+                if (check2) { }
+            }
+            for (int i = 0; i < path.Count(); i++)
+            {
+                if (path[i] != "00") { return false; }
+            }
+            return true;
+        }
+        static bool KnightMove(int x, int y, int a, int b, string[,] board)
+        {
+            if(((Math.Abs(x-a) != 2) && (Math.Abs(y-b) != 1)) || ((Math.Abs(x-a) != 1) && (Math.Abs(y-b) != 2)))
+            {
+                return false;
+            }
+            return true;
+        }
+        static bool PawnMove(int x, int y, int a, int b, string[,] board, string pieceColor)
+        {
+            if(pieceColor == "W")
+            {
+                if (y - b != 1) { return false; }
+                else {  return true; }
+            }
+            else
+            {
+                if (y - b != -1) { return false; }
+                else { return true; }
+            }
+        }
+        static bool PieceColorCheck(int x, int y, int a, int b, string[,] board)
+        {
+            if(board[x, y][0].ToString() == board[a, b][0].ToString()) { return true; }
+            return false;
+        }
+        static bool LegalMove(string[,] board, int turn, string pieceColor, string pieceType, int x, int y, int a, int b)
+        {
+            if (turn % 2 == 0) { if (pieceColor == "B") { return false; } }
+            else { if(pieceColor == "W") { return false; } }
+
+            int checkNewPosition = 0;
+            int oppenentKing = 0;
+
+            
+
+            if (board[a, b] != "00")
+            {
+                if (PieceColorCheck(x, y, a, b, board) == true) { return false; }
+            }
+            switch(pieceType){
+                case "K": return KingMove(x, y, a, b, board);
+                case "Q": return QueenMove(x, y, a, b, board);
+                case "B": return BishopMove(x, y, a, b, board);
+                case "R": return RookMove(x, y, a, b, board);
+                case "N": return KnightMove(x, y, a, b, board);
+                case "P": return PawnMove(x, y, a, b, board, pieceColor);
+                default: break;
+            }
+            return true;
+        }
+    }
+    class ChessPieces
+    {
+        public int x;
+        public int y;
+        public char color;
+        public string piece;
+        public static void Move(int x, int y, int a, int b, string[,] board)
+        {
+            board[a,b] = board[x,y];
+            board[x,y] = "00";
+        }
+    }
+    class Rook : ChessPieces
+    {
+        public char icon = 'R';
+        public Rook(int x, int y, char color, string [,] board)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            board[x,y] = color + icon.ToString();
+        }
+    }
+    class Knight : ChessPieces
+    {
+        public char icon = 'N';
+        public Knight(int x, int y, char color, string[,] board)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            board[x, y] = color + icon.ToString();
+        }
+    }
+    class Bishop : ChessPieces
+    {
+        public char icon = 'B';
+        public Bishop(int x, int y, char color, string[,] board)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            board[x, y] = color + icon.ToString();
+        }
+    }
+    class Queen : ChessPieces
+    {
+        public char icon = 'Q';
+        public Queen(int x, int y, char color, string[,] board)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            board[x, y] = color + icon.ToString();
+        }
+    }
+    class King : ChessPieces
+    {
+        public char icon = 'K';
+        public King(int x, int y, char color, string[,] board)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            board[x, y] = color + icon.ToString();
+        }
+    }
+    class Pawn : ChessPieces
+    {
+        public char icon = 'P';
+        public Pawn(int x, int y, char color, string[,] board)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            board[x, y] = color + icon.ToString();
         }
     }
 }
