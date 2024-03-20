bb513630718167199aee7baf53ff14584bc6ce5a
unknown <peichl294@gmail.com>
2024-03-20 19:35:40 +0100

    Added better UI

--- PaintApp/PaintApp/Form1.Designer.cs
+++ PaintApp/PaintApp/Form1.Designer.cs
@@ -33,6 +33,13 @@
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SizeLabel = new System.Windows.Forms.Label();
+            this.PositionLabel = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -52,7 +59,7 @@
             // 
             this.button2.BackColor = System.Drawing.Color.Black;
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(361, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(50, 50);
             this.button2.TabIndex = 0;
@@ -62,7 +69,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(68, 26);
+            this.comboBox1.Location = new System.Drawing.Point(175, 37);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(51, 24);
             this.comboBox1.TabIndex = 1;
@@ -73,27 +80,82 @@
             // 
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Items.AddRange(new object[] {
-            "Pen",
-            "Eraser"});
-            this.comboBox2.Location = new System.Drawing.Point(125, 26);
+            "Pencil",
+            "Eraser",
+            "Pen"});
+            this.comboBox2.Location = new System.Drawing.Point(73, 38);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(77, 24);
             this.comboBox2.TabIndex = 2;
-            this.comboBox2.Text = "Pen";
+            this.comboBox2.Text = "Pencil";
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(3, 37);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(64, 24);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.PositionLabel);
+            this.panel2.Controls.Add(this.SizeLabel);
+            this.panel2.Location = new System.Drawing.Point(12, 518);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(729, 59);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.comboBox2);
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Controls.Add(this.ClearButton);
+            this.panel3.Location = new System.Drawing.Point(12, 38);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(729, 77);
+            this.panel3.TabIndex = 5;
+            // 
+            // SizeLabel
+            // 
+            this.SizeLabel.AutoSize = true;
+            this.SizeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SizeLabel.Location = new System.Drawing.Point(134, 23);
+            this.SizeLabel.Name = "SizeLabel";
+            this.SizeLabel.Size = new System.Drawing.Size(44, 16);
+            this.SizeLabel.TabIndex = 0;
+            this.SizeLabel.Text = "label1";
+            // 
+            // PositionLabel
+            // 
+            this.PositionLabel.AutoSize = true;
+            this.PositionLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PositionLabel.Location = new System.Drawing.Point(23, 23);
+            this.PositionLabel.Name = "PositionLabel";
+            this.PositionLabel.Size = new System.Drawing.Size(44, 16);
+            this.PositionLabel.TabIndex = 1;
+            this.PositionLabel.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(753, 589);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -105,6 +167,11 @@
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label SizeLabel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label PositionLabel;
     }
 }
 
