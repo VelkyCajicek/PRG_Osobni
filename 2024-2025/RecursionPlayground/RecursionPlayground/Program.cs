e205f4f111bf7632e8c054b26aedd7cf2d87ada8
VelkyCajicek <144894564+VelkyCajicek@users.noreply.github.com>
2024-11-15 09:57:21 +0100

    Random changes

---
+++ 2024-2025/RecursionPlayground/RecursionPlayground/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Factorial(3);
+            int resultFib = Fibonacci(6);
+            Console.WriteLine(result);
+            Console.WriteLine(resultFib);
+            Console.ReadKey();
+        }
+        static int Factorial(int n)
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        static int Fibonacci(int n)
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+            if(n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n-2);
+            }
+        }
+    }
+}
