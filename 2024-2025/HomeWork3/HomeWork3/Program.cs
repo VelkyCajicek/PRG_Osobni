3d760cb6fc8ad0946cf45a71eb5a3c110d784e7d
VelkyCajicek <144894564+VelkyCajicek@users.noreply.github.com>
2025-02-14 15:32:53 +0100

    Started work on HW 3

---
+++ 2024-2025/HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeToEval = @"
+                int test = 0;
+                var count = test + 15;
+                count++;
+                return count;";
+
+            var options = ScriptOptions.Default;
+            var result = CSharpScript.EvaluateAsync(codeToEval, options);
+        }
+    }
+}
